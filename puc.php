2280812d4d6b97ed26c1a4b603784430041c765b
charliecek <charliecek@gmail.com>
2018-03-26 17:53:02 +0200

    v1.1.0: new repos and easier addition of new repos

--- puc.php
+++ puc.php
@@ -4,27 +4,64 @@
  * Description: Uses PUC to check for plugin and theme releases from GitHub
  * Author: charliecek
  * Author URI: http://charliecek.eu/
- * Version: 1.0.0
+ * Version: 1.1.0
  */
 
 require __DIR__.'/plugin-update-checker-4.4/plugin-update-checker.php';
 
 $aPluginSlugs = array(
-  'all-in-one-event-calendar-fixes' => 'all-in-one-event-calendar-fixes',
-  'flashmob-organizer-profile' => 'flashmob-organizer-profile',
+  'all-in-one-event-calendar-fixes' => '%%slug%%',
+  'flashmob-organizer-profile' => '%%slug%%',
+  'flashmob-stats-parser' => '%%slug%%',
+  'polylang_language_fallback' => '%%slug%%',
+  'srd-wp-mail-smtp-fixes' => '%%slug%%',
+  'puc' => '%%slug%%',
 );
 
+$aDefaultProperties = array(
+  'wp-content-path'   => '/plugins/%%slug%%/%%slug%%.php',
+  'github-repo-name'  => '%%slug%%',
+  'github-branch'     => 'master',
+);
 $updateCheckers = array();
-foreach ($aPluginSlugs as $strPluginSlug => $strGitHubRepoName) {
+foreach ($aPluginSlugs as $strPluginSlug => $mixProperties) {
+  if (is_array($mixProperties)) {
+    $aProperties = $mixProperties;
+    // Check format of set properties //
+    foreach ($aProperties as $strKey => $strVal) {
+      if (!is_string($strVal)) {
+        continue 2;
+      }
+    }
+    // Add missing properties //
+    foreach ($aDefaultProperties as $strKey => $strVal) {
+      if (!isset($aProperties[$strKey])) {
+        $aProperties[$strKey] = $strVal;
+      }
+    }
+  } elseif (is_string($mixProperties)) {
+    $aProperties = $aDefaultProperties;
+    $aProperties['github-repo-name'] = $mixProperties;
+  } else {
+    continue;
+  }
   
-  $strPluginPath = dirname(__DIR__)."/plugins/".$strPluginSlug."/".$strPluginSlug.".php";
+  // Replace placeholders //
+  foreach ($aProperties as $strKey => $strVal) {
+    $aProperties[$strKey] = str_replace( '%%slug%%', $strPluginSlug, $strVal );
+    if (strpos($aProperties[$strKey], '%%') !== false) {
+      continue 2;
+    }
+  }
+  
+  $strPluginPath = dirname(__DIR__).$aProperties['wp-content-path'];
   // die("<pre>".var_export(array($strPluginPath, realpath($strPluginPath)), true)."</pre>");
   $updateCheckers[$strPluginSlug] = Puc_v4_Factory::buildUpdateChecker(
-    'https://github.com/charliecek/'.$strGitHubRepoName.'/',
+    'https://github.com/charliecek/'.$aProperties['github-repo-name'].'/',
     $strPluginPath,
     $strPluginSlug
   );
-  $updateCheckers[$strPluginSlug]->setBranch('master');
+  $updateCheckers[$strPluginSlug]->setBranch($aProperties['github-branch']);
 }
 
 //Optional: If you're using a private repository, specify the access token like this:
